7c18f1cbdcb5aee1dd84c439e12ae583faca0a60
pujing <398996745@qq.com>
2026-08-06 02:37:06 +0800

    refactor(build): 更新构建输出文件及资源
    - 修改 index.html 中的脚本引用，更新为新生成的 index-D-0g4oYE.js。
    - 新增多个资源文件，包括关于我的样式、音频转录播放器、阿里云验证码组件等。
    - 删除旧的 ai-msg 相关文件，优化资源管理与构建输出。

---
+++ unpackage/dist/build/web/assets/appGlobalState.B2STW4G6.js
@@ -0,0 +1,1 @@
+import"./index-D-0g4oYE.js";const s=new class{constructor(){this.localAgentList=[],this.searchPlaceholder=""}};export{s as a};
--- unpackage/dist/build/web/assets/appGlobalState.BM4xGyLo.js
+++
@@ -1,1 +0,0 @@
-import"./index-r_r9VbbL.js";const s=new class{constructor(){this.localAgentList=[],this.searchPlaceholder=""}};export{s as a};

7d8220de8c1f7f4d6bfd86ae13a7468080e9b523
pujing <398996745@qq.com>
2026-08-14 19:14:08 +0800

    fix(build): 更新构建资源和优化文件管理
    - 修改 index.html 中的脚本引用，更新为最新的 JavaScript 文件。
    - 新增多个组件和样式文件，增强代理功能展示。
    - 删除不再使用的全局状态管理文件，优化项目资源管理。
    
    Closes #123

---
+++ unpackage/dist/build/web/assets/appGlobalState.CnBf7TTa.js
@@ -0,0 +1,1 @@
+import"./index-qV77q8yT.js";const s=new class{constructor(){this.localAgentList=[],this.searchPlaceholder=""}};export{s as a};
---
+++ unpackage/dist/build/web/assets/safety-zone.uvue_vue_type_script_setup_true_lang.D4L0YM6u.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-qV77q8yT.js";const t=e({__name:"safety-zone",props:{addHeight:{type:Number,default:0}},setup:e=>(e=null,t=null)=>null});export{t as _};

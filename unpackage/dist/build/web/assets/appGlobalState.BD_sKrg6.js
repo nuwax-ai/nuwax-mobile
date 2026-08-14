79b2b783829cd477001ff6a5f287c1fd40eddd72
pujing <398996745@qq.com>
2026-08-15 03:46:26 +0800

    chore(build): 更新构建输出文件和删除无用资源
    - 修改 index.html 中的脚本引用，更新为新生成的文件。
    - 新增多个 JS 和 CSS 文件，优化项目构建体积和整洁性。
    - 删除不再使用的构建输出文件，提升项目整洁性。

---
+++ unpackage/dist/build/web/assets/appGlobalState.BD_sKrg6.js
@@ -0,0 +1,1 @@
+import"./index-ZFXx8M5l.js";const s=new class{constructor(){this.localAgentList=[],this.searchPlaceholder=""}};export{s as a};
--- unpackage/dist/build/web/assets/safety-zone.uvue_vue_type_script_setup_true_lang.FvKXb4dy.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./index-tKvsvpp6.js";const t=e({__name:"safety-zone",props:{addHeight:{type:Number,default:0}},setup:e=>(e=null,t=null)=>null});export{t as _};

881a794ba00a9457bce01943b35b0aeb512362d8
pujing <398996745@qq.com>
2026-08-16 21:56:57 +0800

    chore(build): 更新构建输出文件和删除无用资源
    - 修改 index.html 中的脚本引用，更新为新生成的文件。
    - 新增多个 JS 文件以优化项目功能。
    - 删除不再使用的构建输出文件，提升项目整洁性。
    
    Closes #123

---
+++ unpackage/dist/build/web/assets/common.BE1gVziU.js
@@ -0,0 +1,1 @@
+import"./index-DjNX8tMQ.js";class s{constructor(){this.url="",this.name="",this.type="",this.size=0}}class t{constructor(){this.size=10,this.pageIndex=1,this.keyword=""}}export{t as L,s as U};

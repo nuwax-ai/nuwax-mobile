e1dbe5839262566d3e7e591e83e11f23a220741d
pujing <398996745@qq.com>
2026-08-16 18:56:58 +0800

    chore(nav-bar): 优化导航栏组件和样式
    - 更新 custom-nav-bar 组件，添加固定定位功能以提升 H5 体验。
    - 在 safety-zone 组件中处理 H5 内嵌状态栏高度，确保兼容性。
    - 新增 nav-bar-size.scss 文件，集中管理导航栏高度样式。
    - 更新 agent-detail 页面，调整状态栏高度注入逻辑，优化内嵌 H5 显示。
    
    Closes #123

---
+++ unpackage/dist/build/web/assets/appGlobalState.0js645Cr.js
@@ -0,0 +1,1 @@
+import"./index-BGyXNk7L.js";const s=new class{constructor(){this.localAgentList=[],this.searchPlaceholder=""}};export{s as a};
--- unpackage/dist/build/web/assets/appGlobalState.BdeMlFQ1.js
+++
@@ -1,1 +0,0 @@
-import"./index-U4cx5rYu.js";const s=new class{constructor(){this.localAgentList=[],this.searchPlaceholder=""}};export{s as a};

8d58346e91ab4a5d74739b592667d1bccbd3792a
pujing <398996745@qq.com>
2026-08-21 18:56:51 +0800

    chore: 删除构建后的旧资源文件
    - 移除不再使用的 index.html、JavaScript 和 CSS 文件，清理构建后的资源。
    - 删除多个旧的组件和样式文件，确保项目结构整洁。
    - 更新构建后的资源，准备引入新的功能和组件。

---
+++ unpackage/dist/build/web/assets/appGlobalState.CRveFSzY.js
@@ -0,0 +1,1 @@
+import"./index-B8Px0gvk.js";const s=new class{constructor(){this.localAgentList=[],this.searchPlaceholder=""}};export{s as a};
--- unpackage/dist/build/web/assets/appGlobalState.DO3Uxvl6.js
+++
@@ -1,1 +0,0 @@
-import"./index-9voPf571.js";const s=new class{constructor(){this.localAgentList=[],this.searchPlaceholder=""}};export{s as a};

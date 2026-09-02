9b8ad8f9e6de920bdbbae842800fc74e0d775433
pujing <398996745@qq.com>
2026-09-02 20:29:25 +0800

    fix(build): 更新构建文件和资源
    - 修改 `index.html` 中的脚本引用，更新为新的资源文件名。
    - 删除不再使用的 `acp-permission-card.BhpdMRxI.js` 文件，并新增 `acp-permission-card.DHELqdGT.js`、`agent-component.CszK4Jq0.js`、`agent-icon.DfRLh_e1.js`、`agent.DAw_GK1M.js`、`agreement-checkbox.BWUgOrB8.js`、`ai-msg.B5J_-Guy.js`、`aiMsgMarkdownParser.CuwCMl4d.js`、`aliyun-captcha-h5.uvue_vue_type_script_setup_true_lang.DW_asOL8.js` 相关的构建文件。
    
    Closes #123

--- unpackage/dist/build/web/assets/appGlobalState.CYyxan5X.js
+++
@@ -1,1 +0,0 @@
-import"./index-BKvnJ2zQ.js";const s=new class{constructor(){this.localAgentList=[],this.searchPlaceholder=""}};export{s as a};
---
+++ unpackage/dist/build/web/assets/appGlobalState.DXCW2EtW.js
@@ -0,0 +1,1 @@
+import"./index-BMN6_TNY.js";const s=new class{constructor(){this.localAgentList=[],this.searchPlaceholder=""}};export{s as a};

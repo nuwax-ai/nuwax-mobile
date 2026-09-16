0823ea540c9fbd258dd2867d141d0200a7f2053c
dongdada29 <dongdada29@gmail.com>
2026-09-16 10:57:22 +0800

    chore(build): 合并后重建 web 产物

---
+++ unpackage/dist/build/web/assets/appGlobalState.BrGxUt-u.js
@@ -0,0 +1,1 @@
+import"./index-zltKHBFr.js";const s=new class{constructor(){this.localAgentList=[],this.searchPlaceholder=""}};export{s as a};
--- unpackage/dist/build/web/assets/appGlobalState.DhXSURIm.js
+++
@@ -1,1 +0,0 @@
-import"./index-B6j-Mqep.js";const s=new class{constructor(){this.localAgentList=[],this.searchPlaceholder=""}};export{s as a};

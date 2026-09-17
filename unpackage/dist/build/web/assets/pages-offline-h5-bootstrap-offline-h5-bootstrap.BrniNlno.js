9a59a7185a8657ae9432e237ff872abc6e2d7579
dongdada29 <dongdada29@gmail.com>
2026-09-17 15:53:55 +0800

    chore(build): 重建 web 产物（思考区样式兜底后）

--- unpackage/dist/build/web/assets/pages-offline-h5-bootstrap-offline-h5-bootstrap.BrniNlno.js
+++
@@ -1,1 +0,0 @@
-import{d as s,U as a,b as o,e as n,m as t}from"./index-XkKsssBG.js";import{a as e}from"./uni-app.es.Dztr0nIc.js";import{r,n as l}from"./offline-h5-page.D22B2A1m.js";const p=s({__name:"offline-h5-bootstrap",setup:s=>(e(((s=null)=>{const o=r(s);a((()=>{l(o)}))})),(s=null,a=null)=>{const e=t;return o(),n(e)})});export{p as default};

51a7d6bc599dd32f78a8d9bf15d8680e85d17847
pujing <398996745@qq.com>
2026-09-22 21:07:45 +0800

    fix(build): 更新构建输出文件及样式
    - 修改 index.html 中的 JS 文件引用，更新为新生成的 index-BQD3T7Dd.js
    - 删除不再使用的 acp-permission-card、agent-icon、ai-msg、aiMsgMarkdownParser、aliyun-captcha-h5、appGlobalState、appWebViewImagePreview、appWebViewKeyboardChrome、audio-transcript-player 相关文件
    - 清理构建输出，确保项目整洁

---
+++ unpackage/dist/build/web/assets/appGlobalState.Vc_Kmthq.js
@@ -0,0 +1,1 @@
+import"./index-BQD3T7Dd.js";const e=new class{constructor(){this.localAgentList=[],this.searchPlaceholder="",this.pendingHomeAgentTargetId=0,this.pendingHomeAgentName="",this.pendingHomeAgentIcon=""}};export{e as a};
--- unpackage/dist/build/web/assets/common.Bb0l_rus.js
+++
@@ -1,1 +0,0 @@
-import"./index-BZM6_mN3.js";class s{constructor(){this.url="",this.name="",this.type="",this.size=0}}class t{constructor(){this.size=10,this.pageIndex=1,this.keyword="",this.type=""}}export{t as L,s as U};
